e1f1373d249874dfaa340b74789d4c07fcf84b05
Ynfuien <ynfuien@gmail.com>
2024-03-08 16:40:52 +0100

    Updated hotbar command

--- src/commands/hotbar.command.js
+++ src/commands/hotbar.command.js
@@ -1,75 +1,62 @@
 const { logBot } = require('../utils/logger.js');
 
-// Yeah, maybe later
 module.exports = {
     name: "hotbar",
     enable: true,
-    usage: "<change | use | eat> [slot]",
-    description: "Command for hotbar manage. - WIP",
+    usage: "<set | use | consume | swingarm> [slot]",
+    description: "Command for hotbar manage.",
 
     /**
      * @param {import("..").Main} main
      * @param {string[]} args
      */
-    async run (main, args) {
-        const {bot} = main;
+    async run(main, args) {
+        const { bot } = main;
 
+        if (args.length === 0) return false;
+        const arg1 = args[0].toLowerCase();
 
-        let arg1 = args.shift();
-        if (!arg1) return false;
-        arg1 = arg1.toLowerCase();
-
-        // Change
-        if (arg1 === "change") {
-            if (args.length === 0) {
-                logBot("&cYou have to provide slot to change!");
+        // Set
+        if (arg1 === "set") {
+            if (args.length < 2) {
+                logBot("&cYou have to provide slot to set!");
                 return;
             }
 
-            const slot = parseInt(args.shift());
+            const slot = parseInt(args[1]);
             if (isNaN(slot)) {
                 logBot("&cSlot must be a number!");
                 return;
             }
 
-            if (slot < 0) {
-                logBot("&cSlot can't be lower than 0!");
-                return;
-            }
-
-            if (slot > 8) {
-                logBot("&cSlot can't be higher than 8!");
+            if (slot < 0 || slot > 8) {
+                logBot("&cSlot number must be in range of 0-8!");
                 return;
             }
 
             bot.setQuickBarSlot(slot);
-            logBot(`&bSlot &asuccessfully &bchanged to &3${slot}&b!`);
+            logBot(`&bSlot &asuccessfully &bset to &3${slot}&b!`);
             return;
         }
 
         // Use
         if (arg1 === "use") {
-            const {quickBarSlot} = bot;
+            const { quickBarSlot } = bot;
             let slot = quickBarSlot;
 
-            if (args.length !== 0) {
+            if (args.length > 1) {
                 slot = parseInt(args.shift());
                 if (isNaN(slot)) {
                     logBot("&cSlot must be a number!");
                     return;
                 }
 
-                if (slot < 0) {
-                    logBot("&cSlot can't be lower than 0!");
-                    return;
-                }
-
-                if (slot > 8) {
-                    logBot("&cSlot can't be higher than 8!");
+                if (slot < 0 || slot > 8) {
+                    logBot("&cSlot number must be in range of 0-8!");
                     return;
                 }
             }
-            
+
             if (quickBarSlot !== slot) bot.setQuickBarSlot(slot);
             bot.activateItem();
             bot.deactivateItem();
@@ -79,44 +66,59 @@ module.exports = {
             return;
         }
 
-        // Eat
-        if (arg1 === "eat") {
-            const {quickBarSlot, food} = bot;
-
-            if (food === 20) {
-                logBot("&cFood is full!");
-                return;
-            }
-
+        // Swing arm
+        if (["swingarm", "swing"].includes(arg1)) {
+            const { quickBarSlot } = bot;
             let slot = quickBarSlot;
 
-            if (args.length !== 0) {
+            if (args.length > 1) {
                 slot = parseInt(args.shift());
                 if (isNaN(slot)) {
                     logBot("&cSlot must be a number!");
                     return;
                 }
 
-                if (slot < 0) {
-                    logBot("&cSlot can't be lower than 0!");
-                    return;
-                }
-
-                if (slot > 8) {
-                    logBot("&cSlot can't be higher than 8!");
+                if (slot < 0 || slot > 8) {
+                    logBot("&cSlot number must be in range of 0-8!");
                     return;
                 }
             }
-            
+
             if (quickBarSlot !== slot) bot.setQuickBarSlot(slot);
-            const {heldItem} = bot;
+            bot.swingArm();
+            if (quickBarSlot !== slot) bot.setQuickBarSlot(quickBarSlot);
+
+            logBot(`&aSuccessfully &bswang the hand!`);
+            return;
+        }
+
+        // Consume
+        if (arg1 === "consume") {
+            const { quickBarSlot } = bot;
+
+            let slot = quickBarSlot;
+            if (args.length > 1) {
+                slot = parseInt(args.shift());
+                if (isNaN(slot)) {
+                    logBot("&cSlot must be a number!");
+                    return;
+                }
+
+                if (slot < 0 || slot > 8) {
+                    logBot("&cSlot number must be in range of 0-8!");
+                    return;
+                }
+            }
+
+            if (quickBarSlot !== slot) bot.setQuickBarSlot(slot);
+            const { heldItem } = bot;
             if (heldItem == null) {
                 logBot(`&cThere is no item in slot &4${slot}&c!`);
                 return;
             }
 
             const foods = require('minecraft-data')(bot.version).foodsByName;
-            const {name} = heldItem;
+            const { name } = heldItem;
             if (!foods[name] && name !== "potion") {
                 logBot(`&cItem in slot &4${slot} &ccan't be consumed!`);
                 return;
@@ -125,41 +127,32 @@ module.exports = {
             await bot.consume();
             if (quickBarSlot !== slot) bot.setQuickBarSlot(quickBarSlot);
 
-            logBot(`&bItem in slot &3${slot} &asuccessfully &beaten!`);
+            logBot(`&bItem in slot &3${slot} &asuccessfully &bconsumed!`);
             return;
         }
 
         return false;
     },
-    
+
     /**
      * @param {import("..").Main} main
      * @param {string[]} args
      */
     tabCompletion(main, args) {
-        if (!args) return [];
         if (args.length > 2) return [];
 
+        const subCommands = ["set", "use", "consume", "swingarm"];
+
+        // First arg
         const arg1 = args[0].toLowerCase();
-
-        const subCommands = ["change", "use", "eat"];
-
         if (args.length === 1) {
             return subCommands.filter(element => element.startsWith(arg1));
         }
 
-        if (args.length === 2) {
-            if (!subCommands.includes(arg1)) {
-                return [];
-            }
+        // Second
+        if (!subCommands.includes(arg1)) return [];
 
-            const slotsStrings = [
-                "0", "1", "2", "3", "4", "5", "6", "7", "8"
-            ];
-            
-            return slotsStrings.filter(element => element.startsWith(args[1]));
-        }
-
-        return [];
+        const slotsStrings = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+        return slotsStrings.filter(element => element.startsWith(args[1]));
     }
 }
